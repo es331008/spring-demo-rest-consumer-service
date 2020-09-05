ffef34ed8bff6256cbd94a7d5c45bbbae90f724d
Evan <evan.sorger@gmail.com>
2020-09-05 17:37:55 -0400

    Initial commit

---
+++ src/main/java/com/example/springdemorestconsumerservice/SpringDemoRestConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemorestconsumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoRestConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoRestConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdemorestconsumerservice/SpringDemoRestConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemorestconsumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoRestConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
