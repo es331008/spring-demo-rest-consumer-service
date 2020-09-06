530891eaf01b124887b4af652cd214bd0c1ecab5
Evan <evan.sorger@gmail.com>
2020-09-05 20:34:01 -0400

    Add rest consumer code

---
+++ src/main/java/com/example/springdemorestconsumerservice/model/Value.java
@@ -0,0 +1,14 @@
+package com.example.springdemorestconsumerservice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @NoArgsConstructor @ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Value {
+    private Long id;
+    private String quote;
+}
